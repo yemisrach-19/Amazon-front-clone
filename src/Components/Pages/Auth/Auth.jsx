69c02901187657e34d71db4b024de569d871d5b8
yemisrach-19 <yemi.t.maki@gmail.com>
2025-06-02 13:16:12 -0800

    Autenticatio

--- src/Components/Pages/Auth/Auth.jsx
+++ src/Components/Pages/Auth/Auth.jsx
@@ -3,40 +3,109 @@ import classes from "./Auth.module.css";
 
 import classes from "./SignUp.module.css";
 import { Link } from "react-router-dom";
+import { Auth } from "../1.Utility/firebase";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "Firebase/auth";
+import { DataContext } from "../../Components/DataProvider/DataProvider";
 function Auth() {
-  return (
-    <section className={classes.login}>
-      {/* logo */}
-      <Link>
-        <img
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
-          alt=""
-        />
-      </Link>
-      <div className={classes.login_container}>
-        <h1>Sign In</h1>
-        <form action="">
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" id="email" />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
-          </div>
-          <b utton className={classes.login_signInButton}>Sign In </b>
-        </form>
-        <p>
-          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
-          Sale. Please see our Privacy Notice, our Cookies Notice and our
-          Interest-Based Ads Notice.
-        </p>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [{ user }, dispatch] = useContext(DataContext);
+  console.log(user);
 
-        <button className={classes.login_registerButton}>
-          Create your Amazon Account
-        </button>
-      </div>
-    </section>
-  );
+  const authHandler = async (e) => {
+    e.preventDefault();
+
+    console.log(e.target.name);
+
+    if (e.target.name == "signin") {
+      // firebase auth
+
+      signInWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          dispatch({
+            type: Type.SET_USER,
+
+            user: userInfo.user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userInfo) => {
+          dispatch({
+            type: Type.SET_USER,
+
+            user: userInfo.user,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    return (
+      <section className={classes.login}>
+        {/* logo */}
+        <Link>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png"
+            alt=""
+          />
+        </Link>
+        <div className={classes.login_container}>
+          <h1>Sign In</h1>
+          <form action="">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                id="email"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+
+              <input
+                value={password}
+                onChange={(e) => setEmail(e.target.value)}
+                type="password"
+                id="password"
+              />
+            </div>
+            <button
+              type="submit"
+              onClick={authHandler}
+              name="Sign In"
+              className={classes.login_signInButton}
+            >
+              Sign In{" "}
+            </button>
+          </form>
+          <p>
+            By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+            Sale. Please see our Privacy Notice, our Cookies Notice and our
+            Interest-Based Ads Notice.
+          </p>
+
+          <button
+            type="submit"
+            onClick={authHandler}
+            name="Sign up"
+            className={classes.login_registerButton}
+          >
+            Create your Amazon Account
+          </button>
+        </div>
+      </section>
+    );
+  };
 }
 export default Auth;
--- src/Utility/reducer.js
+++ src/Utility/reducer.js
@@ -1,24 +1,59 @@
 import { Type } from "./action.type";
-
 export const initialState = {
   basket: [],
+  user: null,
 };
 
-export const reduce = (state, action) => {
+export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
-    const existingItem=state.basket.find((item)=>item.id===action.item.id)
-    if(!existingItem )
+      // check if the item exists
+
+      const existingItem = state.basket.find(
+        (item) => item.id === action.item.id
+      );
+
+      if (!existingItem) {
+        return {
+          ...state,
+
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        };
+      } else {
+        const updatedBasket = state.basket.map((item) => {
+          return item.id === action.item.id
+            ? { ...item, amount: item.amount + 1 }
+            : item;
+        });
+
+        return {
+          ...state,
+          basket: updatedBasket,
+        };
+      }
+    case Type.REMOVE_FROM_BASKET:
+      const index = state.basket.findIndex((item) => item.id === action.id);
+
+      let newBasket = [...state.basket];
+
+      if (index >= 0) {
+        if (newBasket[index].amount > 1) {
+          newBasket[index] = {
+            ...newBasket[index],
+            amount: newBasket[index].amount - 1,
+          };
+        } else {
+          newBasket.splice(index, 1);
+        }
+      }
+
       return {
         ...state,
 
-        basket: [...state.basket,{... action.item,amount:1}],
+        basket: newBasket,
       };
-      else{const updateBasket=satisfies.basket.map ((item)=>{return item.id===action.item.id?{...item,amount:item.amount+1}:item})
-      return{...state,basket:updateBasket}}
-      
 
-    default:return state;
+    default:
+      return state;
   }
-
-}
+};
